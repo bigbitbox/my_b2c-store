44b7b5609e1674066b7c6471f500191ced77eaca
rui <ungatz@hotmail.com>
2022-11-12 20:46:25 +0800

    15

---
+++ store-front-user/src/main/java/com/ethercat/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.ethercat.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan(basePackages = "com.ethercat.user.mapper")
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
